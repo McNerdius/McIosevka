211fab50a79826692945755dec22a44620a96b0e
McNerdius <mcnerd@techie.com>
2021-11-23 19:07:55 -0700

    11.0.1 prep

--- build.ps1
+++ build.ps1
@@ -1,2 +1,5 @@
-# docker run -it -v ${PWD}:/build avivace/iosevka-build
+docker run -e FONT_VERSION=11.0.1 -it -v ${PWD}:/build avivace/iosevka-build
 docker run -v "${PWD}/dist/McIosevka/ttf:/in" -v "${PWD}/nerdfont_patched:/out" nerdfonts/patcher --use-single-width-glyphs --adjust-line-height --powerline --powerlineextra --fontawesome --fontawesomeextension --weather --octicons --powersymbols --material --careful --quiet # seti ui & devicons are included by default
+rmdir McIosevka -Recurse -Force
+move .\dist\McIosevka\ .\McIosevka
+rmdir dist -Force
